73a45bd235486526f2f5caceaf108bbee4527eb1
J-Jaeh <1wo2gur@naver.com>
2023-02-08 10:22:43 +0900

    6-[chor] 오운완 board 구조

---
+++ src/main/java/com/team/final8teamproject/board/reository/T_exerciseRepository.java
@@ -0,0 +1,4 @@
+package com.team.final8teamproject.board.reository;
+
+public interface T_exerciseRepository {
+}
---
+++ src/main/java/com/team/final8teamproject/board/service/T_exerciseService.java
@@ -0,0 +1,4 @@
+package com.team.final8teamproject.board.service;
+
+public interface T_exerciseService {
+}
