752d42c03f0c6747a7f9a7c9de6af033de5c53bd
khakyy <khakyy@naver.com>
2023-02-20 18:06:49 +0900

    Test7

---
+++ src/main/java/com/team/final8teamproject/manager/entity/Manager.java
@@ -0,0 +1,62 @@
+package com.team.final8teamproject.manager.entity;
+
+import com.team.final8teamproject.base.entity.BaseEntity;
+import com.team.final8teamproject.user.dto.ProfileModifyRequestDto;
+import com.team.final8teamproject.user.entity.UserRoleEnum;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@DiscriminatorValue(value = "Manager")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "Manager")
+public class Manager extends BaseEntity {
+    private String profileImage;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private Long experience;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum userRoleEnum;
+
+    private boolean applyManager;
+
+    @Builder
+    public Manager(String username, String password, UserRoleEnum role,
+                   String nickName, String phoneNumber, String email,
+                   Long experience) {
+        super(username, password, email, role);
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.experience = experience;
+        this.userRoleEnum = UserRoleEnum.WAIT;
+        applyManager = false;
+    }
+
+    //프로필이미지 수정
+    public void changeManagerProfile(ProfileModifyRequestDto profileModifyRequestDto) {
+        this.nickName = profileModifyRequestDto.getNickname();
+        this.profileImage = profileModifyRequestDto.getImage();
+        this.phoneNumber = profileModifyRequestDto.getPhoneNumber();
+    }
+
+    //권한 변경
+    public void changeRole(UserRoleEnum role) {
+        this.userRoleEnum = role;
+    }
+
+    //신청 상태 변경
+    public void changeApplyStatus(boolean applyManager){
+        this.applyManager = applyManager;
+    }
+
+}
---
+++ src/main/java/com/team/final8teamproject/manager/service/ManagerServiceImpl.java
@@ -0,0 +1,148 @@
+package com.team.final8teamproject.manager.service;
+
+import com.team.final8teamproject.base.entity.BaseEntity;
+import com.team.final8teamproject.base.repository.BaseRepository;
+import com.team.final8teamproject.manager.dto.ManagerResponseDto;
+import com.team.final8teamproject.manager.dto.ManagerSignupRequestDto;
+import com.team.final8teamproject.security.jwt.JwtUtil;
+import com.team.final8teamproject.security.redis.RedisUtil;
+import com.team.final8teamproject.share.exception.CustomException;
+import com.team.final8teamproject.share.exception.ExceptionStatus;
+import com.team.final8teamproject.user.dto.*;
+import com.team.final8teamproject.manager.entity.Manager;
+import com.team.final8teamproject.user.entity.UserRoleEnum;
+import com.team.final8teamproject.manager.repository.ManagerRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ManagerServiceImpl implements ManagerService {
+
+    private final ManagerRepository managerRepository;
+    private final BaseRepository baseRepository;
+    private final RedisUtil redisUtil;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    //1. 회원가입
+    @Transactional
+    public MessageResponseDto signUp(@Valid ManagerSignupRequestDto managerSignupRequestDto) {
+
+        String username = managerSignupRequestDto.getUsername();
+        String password = passwordEncoder.encode(managerSignupRequestDto.getPassword());
+//        String password2 = passwordEncoder.encode(requestDto.getPassword2());
+        String nickName = managerSignupRequestDto.getNickName();
+        String phoneNumber = managerSignupRequestDto.getPhoneNumber();
+        String email = managerSignupRequestDto.getEmail();
+        Long experience = managerSignupRequestDto.getExperience();
+
+        Optional<Manager> found = managerRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+
+        UserRoleEnum userRoleEnum = UserRoleEnum.WAIT;
+        Manager manager = Manager.builder()
+                .username(username).password(password)
+                .nickName(nickName).phoneNumber(phoneNumber)
+                .email(email).experience(experience)
+                .role(userRoleEnum)
+                .build();
+        manager.changeApplyStatus(true);
+        managerRepository.save(manager);
+        return new MessageResponseDto("회원가입 신청 완료");
+    }
+
+    //2. 로그인
+    @Transactional
+    public LoginResponseDto login(LoginRequestDto requestDto) {
+
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        BaseEntity user = baseRepository.findByUsername(username).orElseThrow(
+                () -> new CustomException(ExceptionStatus.WRONG_USERNAME)
+        );
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new CustomException(ExceptionStatus.WRONG_USERNAME);
+        }
+        LoginResponseDto loginResponseDto = jwtUtil.createUserToken(user.getUsername(), user.getRole());
+
+//        if(redisUtil.hasKey("RT:" +user.getUsername())){
+//            throw new SecurityException("이미 접속중인 사용자 입니다.");
+//        }
+        redisUtil.setRefreshToken("RT:" + user.getUsername(), loginResponseDto.getRefreshToken(), loginResponseDto.getRefreshTokenExpirationTime());
+
+        return loginResponseDto;
+    }
+
+
+    //3. 로그아웃
+    @Transactional
+    public String logout(String accessToken, String username) {
+
+        // 레디스에 accessToken 사용못하도록 등록
+        Long expiration = jwtUtil.getExpiration(accessToken);
+        redisUtil.setBlackList(accessToken, "logout", expiration);
+
+        if (redisUtil.hasKey("RT:" + username)) {
+            redisUtil.deleteRefreshToken("RT:" + username);
+        } else {
+            throw new IllegalArgumentException("이미 로그아웃한 유저입니다.");
+        }
+        return "로그아웃 완료";
+    }
+
+    //4. 매니저 가입대기자 조회
+    @Override
+    @Transactional
+    public List<ManagerResponseDto> getStadByList(UserRoleEnum userRoleEnum) {
+
+        List<BaseEntity> list = baseRepository.findByRole(UserRoleEnum.WAIT);
+        List<ManagerResponseDto> managerResponseDtoList = new ArrayList<>();
+
+        for (BaseEntity baseEntity : list) {
+            managerResponseDtoList.add(new ManagerResponseDto((Manager) baseEntity));
+        }
+        return managerResponseDtoList;
+    }
+
+    //5. 매니저 가입 승인
+    @Override
+    @Transactional
+    public void allowStandBy(Long id) {
+        Manager manager = managerRepository.findById(id).orElseThrow();
+        BaseEntity base = baseRepository.findById(id).orElseThrow();
+        if (manager.isApplyManager() == true) {
+            base.changeRole1(UserRoleEnum.MANAGER);
+            manager.changeRole(UserRoleEnum.MANAGER);
+            manager.changeApplyStatus(false);
+        } else {
+            throw new IllegalArgumentException("신청되지 않은 사용자입니다.");
+        }
+    }
+
+    //6. 매니저 가입 거부
+    @Override
+    @Transactional
+    public void refuseStandBy(Long id) {
+        Manager manager = managerRepository.findById(id).orElseThrow();
+        if (manager.isApplyManager() == true) {
+            managerRepository.delete(manager);
+        } else {
+            throw new IllegalArgumentException("신청되지 않은 사용자입니다.");
+        }
+    }
+
+}
