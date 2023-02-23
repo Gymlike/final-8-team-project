4a815fa7b0b4aabdc89defb192b079bd09a32f0f
khakyy <khakyy@naver.com>
2023-02-23 10:05:30 +0900

    Test

--- src/main/java/com/team/final8teamproject/user/dto/ProfileModifyRequestDto.java
+++ src/main/java/com/team/final8teamproject/user/dto/ProfileModifyRequestDto.java
@@ -1,6 +1,7 @@
 package com.team.final8teamproject.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ProfileModifyRequestDto {
-
     @NotBlank(message = "닉네임을 입력하세요.")
     private String nickname;
 
     @NotBlank(message = "이미지를 등록하세요.")
     private String image;
 
-    @NotBlank(message = "핸드폰 번호를 입력하세요.")
-    private String phoneNumber;
+    @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])^[a-zA-Z0-9~!@#$%^&*()+|=]{8,15}$", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9),특수문자(~!@#$%^&*()+|=)")
+    private String password;
 
 }
