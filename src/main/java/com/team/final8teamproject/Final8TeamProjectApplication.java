15ff790dcee27d7b9e607f2c552c313fadab8368
wonizizi89 <wonizizi89@naver.com>
2023-04-08 16:11:38 +0900

    4. [fix] 스케쥴러와 이메일 비동기 처리를 동시 할 경우 충돌 오류 개선

--- src/main/java/com/team/final8teamproject/Final8TeamProjectApplication.java
+++ src/main/java/com/team/final8teamproject/Final8TeamProjectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 public class Final8TeamProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(Final8TeamProjectApplication.class, args);
