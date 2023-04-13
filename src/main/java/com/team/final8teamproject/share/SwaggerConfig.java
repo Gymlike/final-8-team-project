5c0d45b7102be2cb9590a6cec4fb4a2fa2cb8b1a
J-Jaeh <1wo2gur@naver.com>
2023-04-13 13:15:33 +0900

    [feat] swagger 항목별로 분리

--- src/main/java/com/team/final8teamproject/share/SwaggerConfig.java
+++ src/main/java/com/team/final8teamproject/share/SwaggerConfig.java
@@ -33,4 +33,60 @@ public class SwaggerConfig {
                 .pathsToMatch("/todaymeal/**")
                 .build();
     }
+    @Bean
+    public GroupedOpenApi freeBoard(){
+        return GroupedOpenApi.builder()
+                .group("자유게시판")
+                .pathsToMatch("/freeboard/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi contact(){
+        return GroupedOpenApi.builder()
+                .group("문의게시판")
+                .pathsToMatch("/api/contact/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi faq(){
+        return GroupedOpenApi.builder()
+                .group("FAQ")
+                .pathsToMatch("/api/faqs/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi managers(){
+        return GroupedOpenApi.builder()
+                .group("공지사항")
+                .pathsToMatch("/api/managers/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi gym(){
+        return GroupedOpenApi.builder()
+                .group("헬스장조회")
+                .pathsToMatch("/api/gym/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi gymreview(){
+        return GroupedOpenApi.builder()
+                .group("헬스장리뷰")
+                .pathsToMatch("/api/gymreview/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi trainers(){
+        return GroupedOpenApi.builder()
+                .group("트레이너")
+                .pathsToMatch("/api/trainers/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi user(){
+        return GroupedOpenApi.builder()
+                .group("공지사항")
+                .pathsToMatch("/api/user/**")
+                .build();
+    }
 }
