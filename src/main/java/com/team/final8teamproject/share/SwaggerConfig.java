489bb1bf433dd8015abba7cde56654cabdaa066b
J-Jaeh <1wo2gur@naver.com>
2023-04-17 15:52:16 +0900

    [feat] swagger jwt 적용

--- src/main/java/com/team/final8teamproject/share/SwaggerConfig.java
+++ src/main/java/com/team/final8teamproject/share/SwaggerConfig.java
@@ -2,23 +2,46 @@ package com.team.final8teamproject.share;
 
 
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@OpenAPIDefinition(
-        info = @Info(title = "SearchGYM",
-        description = "SearchGYM API 명세",
-        version = "v1")
-)
 
 @Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
 
+        Info info = new Info()
+                .version("v1.0.0")
+                .title(" Search GYM ")
+                .description(" Search GYM API ");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
     @Bean
     public GroupedOpenApi texe(){
         return GroupedOpenApi.builder()
